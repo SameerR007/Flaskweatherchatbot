e9f447334d9361240575f0ee6bbb6742d3718708
SameerR007 <100122935+SameerR007@users.noreply.github.com>
2023-03-20 20:10:33 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,9 +13,12 @@ def index():
     url="https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=013b716d17cad1e8064a6a092a8cf06d".format(lat,lon)
     response=requests.get(url)
     response=response.json()
+    current_temp=round(float(response['main']['temp']-273.15),2)
+    min_temp=round(float(response['main']['temp_min']-273.15),2)
+    max_temp=round(float(response['main']['temp_max']-273.15),2)
     weather=response['weather'][0]['main']
     description=response['weather'][0]['description']
-    final="The weather in {} is mainly {},more specifically {}.".format(source_city,weather,description)
+    final="The weather in {} is mainly {},more specifically {}. Current temprature is approximately {}C with minimum temperature {}C and maximum temperature {}C.".format(source_city,weather,description,current_temp,min_temp,max_temp)
     ft={"fulfillmentText":final}
     return jsonify(ft)
 if __name__=="__main__":
